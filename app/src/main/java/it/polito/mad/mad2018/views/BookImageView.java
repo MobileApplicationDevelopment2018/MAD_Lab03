1a680d5bbfb214528d8ae98299d7974e04e5945d
EugenioGallea <gallea6@gmail.com>
2018-04-24 02:29:48 +0200

    Fixed bug that picture appeared for wrong books. Still has to be checked the fact that the default book preview is not displayed.

--- app/src/main/java/it/polito/mad/mad2018/views/BookImageView.java
+++ app/src/main/java/it/polito/mad/mad2018/views/BookImageView.java
@@ -3,16 +3,18 @@ package it.polito.mad.mad2018.views;
 import android.content.Context;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.algolia.instantsearch.ui.views.AlgoliaHitView;
-import com.bumptech.glide.Glide;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import it.polito.mad.mad2018.R;
 import it.polito.mad.mad2018.data.Book;
+import it.polito.mad.mad2018.utils.GlideApp;
 
 public class BookImageView extends AppCompatImageView implements AlgoliaHitView {
     public BookImageView(Context context) {
@@ -31,19 +33,29 @@ public class BookImageView extends AppCompatImageView implements AlgoliaHitView 
     public void onUpdateView(JSONObject result) {
         try {
             String bookId = result.getString("bookId");
-            StorageReference ref = FirebaseStorage.getInstance().getReference()
-                    .child(Book.FIREBASE_STORAGE_BOOKS_FOLDER)
-                    .child(bookId)
-                    .child(Book.FIREBASE_STORAGE_IMAGE_NAME);
+            Log.d("Tag", bookId + "=>" + result.getString("title"));
+            if (bookId != null) {
+                StorageReference ref = FirebaseStorage.getInstance().getReference()
+                        .child(Book.FIREBASE_STORAGE_BOOKS_FOLDER)
+                        .child(bookId)
+                        .child(Book.FIREBASE_STORAGE_IMAGE_NAME);
 
-            if (ref != null) {
-                Glide.with(getContext())
-                        .load(ref)
+                if (ref != null) {
+                    GlideApp.with(getContext())
+                            .load(ref)
+                            .into(this);
+                }
+            } else {
+                GlideApp.with(getContext())
+                        .load(R.drawable.default_book_preview)
                         .into(this);
             }
 
         } catch (JSONException e) {
             // Non existing field, don't do anything
+            GlideApp.with(getContext())
+                    .load(R.drawable.default_book_preview)
+                    .into(this);
         }
     }
 }
